37ba0b96477490c44c2974ed870170a828ff31c4
chaijunjie <1109566870@qq.com>
2020-11-19 14:29:43 +0800

    list使用流去除重复的数据

---
+++ ideaspringboot/IDEAWork/src/jdknew/distinctList.java
@@ -0,0 +1,71 @@
+package jdknew;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+/**
+ * @Auther: 柴俊杰
+ * @Description: 按照某个字段去除重复的值并返回一个List
+ * @Date: 2020/11/19 14:07
+ */
+public class distinctList {
+	public static void main(String[] args) {
+		List<Person> list = new ArrayList<>();
+		for(int i = 0;i< 1000;i++){
+			Person p = new Person();
+			p.setAddress("s"+i/2);
+			p.setAge((i/2)+"");
+			p.setEmail("");
+			p.setName(i+"");
+			list.add(p);
+		}
+		ArrayList<Person> collect = list.stream().collect(Collectors.collectingAndThen(
+				Collectors.toCollection(() -> new TreeSet<>(
+						Comparator.comparing(
+								Person::getAge))), ArrayList::new));
+		System.out.println(collect.size());
+		ArrayList<Person> collect1 = list.stream().collect(Collectors.collectingAndThen(
+				Collectors.toCollection(() -> new TreeSet<>(
+						Comparator.comparing(
+								person -> person.getAge() + ";" + person.getAddress()))), ArrayList::new));
+		System.out.println(collect1.size());
+	}
+}
+class Person{
+	private String name;
+	private String age;
+	private String address;
+	private String email;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAge() {
+		return age;
+	}
+
+	public void setAge(String age) {
+		this.age = age;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+}
